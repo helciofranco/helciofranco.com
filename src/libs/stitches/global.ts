5300d6bd80f61d60aece5ac82da46eabe2dd5ec7
Hélcio Franco <helciofranco@me.com>
2022-07-08 20:20:56 -0400

    feat: add Biotif font family

--- src/libs/stitches/global.ts
+++ src/libs/stitches/global.ts
@@ -3,57 +3,46 @@ import { globalCss } from '.';
 export const globalStyles = globalCss({
   '*': {
     fontFamily: '$body',
+    // Change from `box-sizing: content-box` so that `width`
+    // is not affected by `padding` or `border`.
+    boxSizing: 'border-box',
   },
   'html, body': {
-    margin: '0',
-    padding: '0',
+    margin: '0', // Remove the margin in all browsers.
+    padding: '0', // Remove the padding in all browsers.
     WebkitFontSmoothing: 'antialiased',
     background: '$background',
   },
-  // '@font-face': [
-  //   {
-  //     fontFamily: 'Neuzeit Grotesk Bold',
-  //     src: `url("/static/font/NeuzeitGrotesk-Bold.woff2") format("woff2"),
-  //       url("/static/font/NeuzeitGrotesk-Bold.woff") format("woff")`,
-  //     fontWeight: 'normal',
-  //     fontStyle: 'normal',
-  //   },
-  //   {
-  //     fontFamily: 'Fira Code',
-  //     src: `url("/static/font/FiraCode-Regular.woff2") format("woff2"),
-  //       url("/static/font/FiraCode-Regular.woff") format("woff")`,
-  //     fontWeight: 'normal',
-  //     fontStyle: 'normal',
-  //   },
-  //   {
-  //     fontFamily: 'Biotif',
-  //     src: `url("/static/font/Biotif-Bold.woff2") format("woff2"),
-  //       url("/static/font/Biotif-Bold.woff") format("woff")`,
-  //     fontWeight: 'bold',
-  //     fontStyle: 'normal',
-  //   },
-  //   {
-  //     fontFamily: 'Biotif',
-  //     src: `url("/static/font/Biotif-Book.woff2") format("woff2"),
-  //       url("/static/font/Biotif-Book.woff") format("woff")`,
-  //     fontWeight: 500,
-  //     fontStyle: 'normal',
-  //   },
-  //   {
-  //     fontFamily: 'Biotif',
-  //     src: `url("/static/font/Biotif-Regular.woff2") format("woff2"),
-  //       url("/static/font/Biotif-Regular.woff") format("woff")`,
-  //     fontWeight: 'normal',
-  //     fontStyle: 'normal',
-  //   },
-  //   {
-  //     fontFamily: 'Biotif',
-  //     src: `url("/static/font/Biotif-RegularItalic.woff2") format("woff2"),
-  //       url("/static/font/Biotif-RegularItalic.woff") format("woff")`,
-  //     fontWeight: 'normal',
-  //     fontStyle: 'italic',
-  //   },
-  // ],
+  '@font-face': [
+    {
+      fontFamily: 'Biotif',
+      src: `url("/fonts/Biotif-Bold.woff2") format("woff2"),
+        url("/fonts/Biotif-Bold.woff") format("woff")`,
+      fontWeight: 'bold',
+      fontStyle: 'normal',
+    },
+    {
+      fontFamily: 'Biotif',
+      src: `url("/fonts/Biotif-Book.woff2") format("woff2"),
+        url("/fonts/Biotif-Book.woff") format("woff")`,
+      fontWeight: 500,
+      fontStyle: 'normal',
+    },
+    {
+      fontFamily: 'Biotif',
+      src: `url("/fonts/Biotif-Regular.woff2") format("woff2"),
+        url("/fonts/Biotif-Regular.woff") format("woff")`,
+      fontWeight: 'normal',
+      fontStyle: 'normal',
+    },
+    {
+      fontFamily: 'Biotif',
+      src: `url("/fonts/Biotif-RegularItalic.woff2") format("woff2"),
+        url("/fonts/Biotif-RegularItalic.woff") format("woff")`,
+      fontWeight: 'normal',
+      fontStyle: 'italic',
+    },
+  ],
 });
 
 globalStyles();
