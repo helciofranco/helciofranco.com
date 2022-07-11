6ac501903a8a9f902f62e22cfe083bfe418cb496
Hélcio Franco <helciofranco@me.com>
2022-07-10 21:05:32 -0400

    chore: add text examples

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -5,6 +5,10 @@ const Home = () => {
   return (
     <Box>
       <Text variant="h1">h1. Headline</Text>
+      <Text variant="h2">h2. Headline</Text>
+      <Text variant="h3">h3. Headline</Text>
+      <Text variant="h4">h4. Headline</Text>
+      <Text variant="h5">h5. Headline</Text>
       <Text variant="h6">h6. Headline</Text>
       <Text color="secondary">Body text</Text>
     </Box>
