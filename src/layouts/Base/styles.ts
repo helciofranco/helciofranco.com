fe61f4463f87c564bc3cbd83ccf26d3f222570ec
Hélcio Franco <helciofranco@me.com>
2022-07-10 22:56:14 -0400

    feat: add horizontal padding on base layout

--- src/layouts/Base/styles.ts
+++ src/layouts/Base/styles.ts
@@ -2,5 +2,7 @@ import { styled } from '@libs/stitches';
 
 export const Wrapper = styled('div', {
   maxWidth: '800px',
+  paddingLeft: '$space8',
+  paddingRight: '$space8',
   margin: '0 auto',
 });
