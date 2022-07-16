96098ddfa6907d218524bcb657d5c7ced33fa7c5
Hélcio Franco <helciofranco@me.com>
2022-07-15 23:12:08 -0400

    chore: just testing

--- src/modules/blog/screens/Feed/index.tsx
+++ src/modules/blog/screens/Feed/index.tsx
@@ -83,6 +83,120 @@ const Feed = () => {
           The quick brown fox jumps over the lazy dog.
         </Text>
 
+        <Text variant="h2" as="h2" marginBottom>
+          The stack
+        </Text>
+
+        <Text marginBottom>
+          This blog is a <Link href="aaaaa">Next.js application</Link>.
+        </Text>
+
+        <Text marginBottom>
+          With Next, you have a few different options when it comes to page
+          rendering: you can choose to do it “on-demand” (
+          <b>server-side rendering</b>) or ahead of time (
+          <b>static site generation</b>). I&rsquo;ve opted to build all the blog
+          posts ahead of time, when the site is generated.
+        </Text>
+
+        <Text marginBottom>
+          I also use Next&rsquo;s API Routes for things that require persistence
+          in the backend. I use MongoDB as my database, to store stuff like the
+          # of likes each post has.
+        </Text>
+
+        <Text marginBottom>
+          I deploy this blog on Vercel. I initially chose them because
+          they&rsquo;re the company behind Next.js, and I figured it would be
+          well-optimized. Honestly, their platform is awesome. I wound up moving
+          some of my non-Next projects there as well.
+        </Text>
+
+        <Text marginBottom>
+          When it comes to the styling, I use styled-components, and write all
+          the styles from scratch. I don&rsquo;t use any &rdquo;cosmetic&rdquo;
+          libraries like Bootstrap (and I don&rsquo;t think you should either).
+          I do use Reach UI for things like modals, though.
+        </Text>
+
+        <Text marginBottom>
+          For animation, I mainly rely on React Spring, though I&rsquo;ve
+          started dabbling with Framer Motion recently.
+        </Text>
+
+        <Text marginBottom>But the most critical part of my stack is MDX.</Text>
+
+        <Text marginBottom>
+          Lets say you have a <Code>PackingList</Code> component rendering
+          several Items, which can be marked as packed or not:
+        </Text>
+
+        <Text color="secondary" serif marginBottom>
+          The quick brown fox jumps over the lazy dog. <Code>if</Code> and{' '}
+          <Code>{'&&'}</Code>
+        </Text>
+
+        <Text serif marginBottom>
+          The quick brown fox jumps over the lazy dog.
+        </Text>
+
+        <Text variant="h2" as="h2" marginBottom>
+          The stack
+        </Text>
+
+        <Text marginBottom>
+          This blog is a <Link href="aaaaa">Next.js application</Link>.
+        </Text>
+
+        <Text marginBottom>
+          With Next, you have a few different options when it comes to page
+          rendering: you can choose to do it “on-demand” (
+          <b>server-side rendering</b>) or ahead of time (
+          <b>static site generation</b>). I&rsquo;ve opted to build all the blog
+          posts ahead of time, when the site is generated.
+        </Text>
+
+        <Text marginBottom>
+          I also use Next&rsquo;s API Routes for things that require persistence
+          in the backend. I use MongoDB as my database, to store stuff like the
+          # of likes each post has.
+        </Text>
+
+        <Text marginBottom>
+          I deploy this blog on Vercel. I initially chose them because
+          they&rsquo;re the company behind Next.js, and I figured it would be
+          well-optimized. Honestly, their platform is awesome. I wound up moving
+          some of my non-Next projects there as well.
+        </Text>
+
+        <Text marginBottom>
+          When it comes to the styling, I use styled-components, and write all
+          the styles from scratch. I don&rsquo;t use any &rdquo;cosmetic&rdquo;
+          libraries like Bootstrap (and I don&rsquo;t think you should either).
+          I do use Reach UI for things like modals, though.
+        </Text>
+
+        <Text marginBottom>
+          For animation, I mainly rely on React Spring, though I&rsquo;ve
+          started dabbling with Framer Motion recently.
+        </Text>
+
+        <Text marginBottom>But the most critical part of my stack is MDX.</Text>
+
+        <Text marginBottom>
+          Lets say you have a <Code>PackingList</Code> component rendering
+          several Items, which can be marked as packed or not:
+        </Text>
+
+        <Text color="secondary" serif marginBottom>
+          The quick brown fox jumps over the lazy dog. <Code>if</Code> and{' '}
+          <Code>{'&&'}</Code>
+        </Text>
+
+        <Text serif marginBottom>
+          The quick brown fox jumps over the lazy dog.
+        </Text>
+
         <Card>
           <Text variant="h3" as="h3" marginBottom>
             You will learn <Code>if</Code> and <Code>{'&&'}</Code>
