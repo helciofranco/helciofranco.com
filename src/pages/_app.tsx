b738c0ff2b7f0d1b7a25614ea0454155e09e2d8c
Hélcio Franco <helciofranco@me.com>
2022-07-15 23:17:45 -0400

    feat: use system fonts instead of webfonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,6 @@ import type { AppProps } from 'next/app';
 
 import Base from '@layouts/Base';
 
-import '@libs/stitches/font-faces.css';
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Base>
