624a18eab51dcbde1d98fbd5f7f9c95729fb9150
Hélcio Franco <helciofranco@me.com>
2022-07-24 14:24:31 -0400

    fix: adjust post container spacing

--- src/modules/blog/components/PostContainer/index.ts
+++ src/modules/blog/components/PostContainer/index.ts
@@ -3,47 +3,20 @@ import { styled } from '@libs/stitches';
 import { Container as PostInfoContainer } from '@blog/components/PostInfo/styles';
 
 const PostContainer = styled('article', {
-  // Header and subheaders, default for everyone
+  // Header and subheaders
   ['& h1, h2, h3']: {
     fontWeight: '$bold',
   },
 
-  // Default PostInfo spacing
+  // Spacing between subheaders and paragraphs
+  ['& h2, h3, p']: {
+    marginBottom: '$space10',
+  },
+
+  // PostInfo spacing
   [`& ${PostInfoContainer}`]: {
     marginTop: '$space2',
-  },
-
-  variants: {
-    marginBottom: {
-      // Usually used on feed
-      sm: {
-        // PostInfo spacing
-        [`& ${PostInfoContainer}`]: {
-          marginBottom: '$space4',
-        },
-
-        // Spacing between subheaders and paragraphs
-        ['& h2, h3, p']: {
-          marginBottom: '$space0',
-        },
-      },
-
-      // Usually used on posts
-      regular: {
-        // PostInfo spacing
-        [`& ${PostInfoContainer}`]: {
-          marginBottom: '$space16',
-        },
-
-        // Spacing between subheaders and paragraphs
-        ['& h2, h3, p']: {
-          marginBottom: '$space10',
-        },
-      },
-    },
-  },
-  defaultVariants: {
-    marginBottom: 'regular',
+    marginBottom: '$space16',
   },
 });
 
