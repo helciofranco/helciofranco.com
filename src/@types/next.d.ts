e3f5682fdf756668be232586d8c672b02329bb95
Hélcio Franco <helciofranco@me.com>
2022-07-24 15:55:36 -0400

    feat: add possibility to pass parameters to nextpage

--- src/@types/next.d.ts
+++ src/@types/next.d.ts
@@ -2,7 +2,7 @@ import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
 
 declare module 'next/app' {
-  export type Layout = NextPage & {
+  export type Layout<T = {}> = NextPage<T> & {
     getLayout?: (page: ReactElement) => ReactNode;
   };
 
