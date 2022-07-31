fbc507970acdc7399a9f00861902f5e0bde15222
Hélcio Franco <helciofranco@me.com>
2022-07-31 18:24:34 -0400

    feat: add bottom spacing after all <pre> element

--- src/modules/blog/components/Details/PostContainer/index.ts
+++ src/modules/blog/components/Details/PostContainer/index.ts
@@ -3,8 +3,8 @@ import { styled } from '@libs/stitches';
 import { Container as PostInfoContainer } from '@blog/components/Details/PostInfo/styles';
 
 const PostContainer = styled('article', {
-  // Spacing between subheaders and paragraphs
-  ['& h2, h3, p']: {
+  // Spacing between subheaders, paragraphs and snippets
+  ['& h2, h3, p, pre']: {
     marginBottom: '$space10',
   },
 
